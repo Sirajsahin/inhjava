1270061ad8014df36aac3640a2b6cc49ca0c4fec
Sirajsahin <96188626+Sirajsahin@users.noreply.github.com>
2021-12-31 21:58:11 +0530

    Update circle.js

--- circle.js
+++ circle.js
@@ -1,6 +1,28 @@
-import Shape from './shape.js'
 
- class Circle extends Shape {
-   calculateArea(){
-   }
+
+
+
+   
+
+
+
+var Shape = require('./shape.js');
+
+// declare class
+
+class Circle extends Shape {
+
+     constructor(color) {
+
+          super();
+
+     }
+
+     calculateArea() {
+
+     }
+
 }
+
+// export class using module.exports
+
